a0513b68b42913357de56d97b93e358ea8a17b8c
jpcaulfi <joecaulfield29@yahoo.com>
2022-06-14 23:24:06 -0400

    server skeleton

--- api/src/controller.ts
+++ api/src/controller.ts
@@ -1,15 +1,56 @@
+import * as anchor from "@project-serum/anchor";
 import { Request, Response, Router } from 'express';
 import axios, { AxiosResponse } from 'axios'; // Needed for client calls
-import { getNftsMinted } from "./service";
+import * as service from "./service";
 
 const router = Router();
 
-/// Look up any NFTs that have been minted by our collection.
-
-router.get('/nfts-minted', async (req: Request, res: Response) => {
-    let nftsMinted: string[] = [];
-    return res.status(200).json({nfts : await getNftsMinted()});
+// Mint a new random NFT from our collection.
+//
+router.get('/mint-new', async (req: Request, res: Response) => {
+    return res.status(200).json(
+        await service.mintNFt()
+    );
 });
 
+// Look up any NFTs that have been minted by our collection.
+//
+router.get('/get-nfts-minted', async (req: Request, res: Response) => {
+    return res.status(200).json(
+        await service.getNftsMinted()
+    );
+});
+
+// Place a bid on an NFT.
+//
+router.post('/bid-on-nft', async (req: Request, res: Response) => {
+    return res.status(200).json(
+        await service.bidOnNft(
+            req.body.mintAddress,
+            req.body.lamports,
+        )
+    );
+});
+
+// Get all NFTs owned by your wallet.
+//
+router.get('/get-nfts-owned/:publicKey', async (req: Request, res: Response) => {
+    return res.status(200).json(
+        await service.getNftsOwned(
+            new anchor.web3.PublicKey(req.params.publicKey)
+        )
+    );
+});
+
+// Sell an NFT to the highest bidder.
+//
+router.post('/sell-nft', async (req: Request, res: Response) => {
+    return res.status(200).json(
+        await service.sellNft(
+            req.body.mintAddress,
+            
+        )
+    );
+});
 
 export = router;
--- api/src/service.ts
+++ api/src/service.ts
@@ -1,9 +1,52 @@
+import * as anchor from "@project-serum/anchor";
 
 
 export type Nft = JSON;
 
 
+// Mint a new random NFT from our collection.
+//
+export async function mintNFt(): Promise<Nft[]> {
+    let x: Nft[] = [];
+    return x;
+}
+
+// Look up any NFTs that have been minted by our collection.
+//
 export async function getNftsMinted(): Promise<Nft[]> {
     let x: Nft[] = [];
     return x;
 }
+
+// Place a bid on an NFT.
+//
+export async function bidOnNft(
+    mintAddress: anchor.web3.PublicKey,
+    lamports: number,
+): Promise<Nft[]> {
+
+    let x: Nft[] = [];
+    return x;
+}
+
+// Get all NFTs owned by your wallet.
+//
+export async function getNftsOwned(
+    publicKey: anchor.web3.PublicKey
+): Promise<Nft[]> {
+    
+    let x: Nft[] = [];
+    return x;
+}
+
+// Sell an NFT to the highest bidder.
+//
+export async function sellNft(
+    mintAddress: anchor.web3.PublicKey,
+    ownerPublicKey: anchor.web3.PublicKey,
+): Promise<Nft[]> {
+    
+    let x: Nft[] = [];
+    return x;
+}
+
