c92abd22c7c30a429ab91add6e7ae17572521755
jpcaulfi <jcaulfield135@gmail.com>
2022-06-16 22:58:23 -0400

    mint program done

--- programs/nft-marketplace/src/mint.rs
+++ programs/nft-marketplace/src/mint.rs
@@ -4,7 +4,6 @@ use {
         solana_program::{
             native_token::LAMPORTS_PER_SOL,
             program::invoke,
-            system_instruction::create_account,
         },
         system_program,
     },
@@ -12,8 +11,10 @@ use {
         associated_token,
         token,
     },
-    mpl_token_metadata::instruction::{
-        create_master_edition_v3, create_metadata_accounts_v2
+    mpl_token_metadata::{
+        ID as TOKEN_METADATA_ID,
+        instruction,
+        state,
     },
 };
 
@@ -24,13 +25,71 @@ pub fn mint(
     metadata_uri: String,
 ) -> Result<()> {
 
-    msg!("Initializing...");
+    msg!("Minting token...");
+    msg!("Token: {}", &ctx.accounts.mint.to_account_info().key());    
+    token::mint_to(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo{
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.token_account.to_account_info(),
+                authority: ctx.accounts.mint_authority.to_account_info(),
+            },
+        ),
+        1,
+    )?;
 
-    // Create Mint
-    let mint = ctx.accounts.mint.to_account_info();
-    msg!("Token generated: {}", &mint.key());
+    msg!("Creating metadata...");
+    msg!("Metadata address: {}", &ctx.accounts.metadata.to_account_info().key());
+    invoke(
+        &instruction::create_metadata_accounts_v2(
+            TOKEN_METADATA_ID, 
+            ctx.accounts.metadata.key(), 
+            ctx.accounts.mint.key(), 
+            ctx.accounts.mint_authority.key(), 
+            ctx.accounts.mint_authority.key(), 
+            ctx.accounts.mint_authority.key(), 
+            metadata_title, 
+            metadata_symbol, 
+            metadata_uri, 
+            None,
+            1,
+            true, 
+            false, 
+            None, 
+            None,
+        ),
+        &[
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.token_account.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
+    )?;
 
-    // Create Associated Token Account
+    msg!("Creating master edition metadata...");
+    msg!("Master edition address: {}", &ctx.accounts.master_edition.to_account_info().key());
+    invoke(
+        &instruction::create_master_edition_v3(
+            TOKEN_METADATA_ID, 
+            ctx.accounts.master_edition.key(), 
+            ctx.accounts.mint.key(), 
+            ctx.accounts.mint_authority.key(), 
+            ctx.accounts.mint_authority.key(), 
+            ctx.accounts.metadata.key(), 
+            ctx.accounts.mint_authority.key(), 
+            Some(0),
+        ),
+        &[
+            ctx.accounts.master_edition.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.token_account.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
+    )?;
 
     msg!("Token mint process completed.");
 
@@ -40,14 +99,21 @@ pub fn mint(
 
 #[derive(Accounts)]
 pub struct MintNft<'info> {
-    #[account(
-        init, 
-        payer = authority, 
-        space = 8 + 40,
-    )]
+    /// CHECK: Metaplex will check this
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+    /// CHECK: Metaplex will check this
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
+    #[account(mut)]
     pub mint: Account<'info, token::Mint>,
     #[account(mut)]
-    pub authority: Signer<'info>,
+    pub token_account: Account<'info, token::TokenAccount>,
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, token::Token>,
+    /// CHECK: Metaplex will check this
+    pub token_metadata_program: UncheckedAccount<'info>,
 }
--- tests/nft-marketplace.ts
+++ tests/nft-marketplace.ts
@@ -1,9 +1,18 @@
 import * as anchor from "@project-serum/anchor";
 // ** Comment this to use solpg imported IDL **
+import { 
+  TOKEN_PROGRAM_ID, 
+  createAssociatedTokenAccountInstruction, 
+  getAssociatedTokenAddress, 
+  createInitializeMintInstruction, 
+  MINT_SIZE 
+} from '@solana/spl-token';
 import { NftMarketplace } from "../target/types/nft_marketplace";
+
 
 describe("nft-marketplace", async () => {
   
+
   const provider = anchor.AnchorProvider.env()
   const wallet = provider.wallet as anchor.Wallet;
   anchor.setProvider(provider);
@@ -15,54 +24,82 @@ describe("nft-marketplace", async () => {
   // );
   // ** Comment this to use solpg imported IDL **
   const program = anchor.workspace.NftMarketplace as anchor.Program<NftMarketplace>;
-  
+
   const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
     "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
   );
 
-  const mintKeypair: anchor.web3.Keypair = anchor.web3.Keypair.generate();
-  // const tokenAddress = await anchor.utils.token.associatedAddress({
-  //   mint: mintKeypair.publicKey,
-  //   owner: wallet.publicKey
-  // });
-
-  // const metadataAddress = (await anchor.web3.PublicKey.findProgramAddress(
-  //   [
-  //     Buffer.from("metadata"),
-  //     TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-  //     mintKeypair.publicKey.toBuffer(),
-  //   ],
-  //   TOKEN_METADATA_PROGRAM_ID
-  // ))[0];
-  // const masterEditionAddress = (await anchor.web3.PublicKey.findProgramAddress(
-  //   [
-  //     Buffer.from("metadata"),
-  //     TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-  //     mintKeypair.publicKey.toBuffer(),
-  //     Buffer.from("edition"),
-  //   ],
-  //   TOKEN_METADATA_PROGRAM_ID
-  // ))[0];
 
   it("Mint!", async () => {
+
+    const mintKeypair: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    const tokenAddress = await anchor.utils.token.associatedAddress({
+      mint: mintKeypair.publicKey,
+      owner: wallet.publicKey
+    });
+    const requiredLamports: number = await program.provider.connection.getMinimumBalanceForRentExemption(
+      MINT_SIZE
+    );
+    console.log(`New token: ${mintKeypair.publicKey}`);
+
+    await program.provider.sendAndConfirm(
+      new anchor.web3.Transaction().add(
+        anchor.web3.SystemProgram.createAccount({
+          fromPubkey: wallet.publicKey,
+          newAccountPubkey: mintKeypair.publicKey,
+          space: MINT_SIZE,
+          programId: TOKEN_PROGRAM_ID,
+          lamports: requiredLamports,
+        }),
+        createInitializeMintInstruction(
+          mintKeypair.publicKey,
+          0,
+          wallet.publicKey,
+          wallet.publicKey
+        ),
+        createAssociatedTokenAccountInstruction(
+          wallet.publicKey,
+          tokenAddress,
+          wallet.publicKey,
+          mintKeypair.publicKey
+        )
+      ), 
+      [mintKeypair]
+    );
+    console.log("Mint initialized");
+
+    const metadataAddress = (await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("metadata"),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mintKeypair.publicKey.toBuffer(),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    ))[0];
+    console.log("Metadata initialized");
+
+    const masterEditionAddress = (await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("metadata"),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mintKeypair.publicKey.toBuffer(),
+        Buffer.from("edition"),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    ))[0];
+    console.log("Master edition metadata initialized");
     
-    const tx = await program.methods.mint(
+    await program.methods.mint(
       "Test NFT", "TEST", "https://raw.githubusercontent.com/Coding-and-Crypto/Rust-Solana-Tutorial/master/nft-marketplace/nft-example.json"
     )
     .accounts({
-      // masterEdition: masterEditionAddress,
-      // metadata: metadataAddress,
+      masterEdition: masterEditionAddress,
+      metadata: metadataAddress,
       mint: mintKeypair.publicKey,
-      // mintAuthority: wallet.publicKey,
-      // rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      authority: wallet.publicKey,
-      // systemProgram: anchor.web3.SystemProgram.programId,
-      // tokenAccount: tokenAddress,
-      // tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
-      // tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+      tokenAccount: tokenAddress,
+      mintAuthority: wallet.publicKey,
+      tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
     })
-    .signers([mintKeypair])
     .rpc();
-    console.log("Your transaction signature", tx);
   });
 });
